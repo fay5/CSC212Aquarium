6b0d578f5897906f6be4c21b7410e3203f050de7
fadumoadan <fadumoadan@131.229.197.234>
2019-09-13 14:24:51 -0400

    Fish homework.

---
+++ src/main/java/edu/smith/cs/csc212/aquarium/Fish.java
@@ -0,0 +1,35 @@
+package edu.smith.cs.csc212.aquarium;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class Fish {
+	Color color;
+	int x;
+	int y; 
+	
+	public Fish(Color c, int startX, int startY) {
+		this.color = c;
+		this.x = startX;
+		this.y = startY;
+}
+	
+	public void draw (Graphics2D window) {
+		DrawFish.facingRight(window, 
+				this.color, this.x, this.y);
+		//BIG FISH 
+		
+		DrawFish.facingLeft(window,this.color, this.x, this.y);
+		// Draw the confused fish!
+		DrawFish.facingRight(window, this.color, this.x, this.y);
+		
+		//SMALLER FISH
+		DrawFish.smallFacingLeft(window,this.color, this.x, this.y);
+		// Draw the confused fish!
+		DrawFish.smallFacingRight(window, this.color, this.x, this.y);
+	}
+	
+	public void swim() {
+		this.y +=1;
+	}
+}
